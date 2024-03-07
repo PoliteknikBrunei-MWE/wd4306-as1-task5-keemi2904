89a752e66a88ad1ed34250ea31267b1c0dc2cf61
HakeemAzeez <22ftt1511@student.pb.edu.bn>
2024-03-07 15:22:01 +0800

    added auth + link of auth from places page in tab1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
+  },
 ];
